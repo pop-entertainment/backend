43a78a3895c946401ca2b8f09db2cdbdd0c16a34
osipov_de2 <osipov_de2@albank.ru>
2025-04-17 19:15:04 +0900

    Add automigration

--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -40,6 +40,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<StoreDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
